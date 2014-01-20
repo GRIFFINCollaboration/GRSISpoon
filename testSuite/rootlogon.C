9c421fadd317717e79b15d5ba3e791292ef7755f
P.C. Bender <pcbend@gmail.com>
2014-01-20 11:55:13 -0800

    Updated testGaunlet.C to make more readable.

--- testSuite/rootlogon.C
+++ testSuite/rootlogon.C
@@ -1,6 +1,6 @@
 {
 	gSystem->Load("$ROOTSYS/lib/libPhysics.so");   // need to make tvector3 play nice.
-	gSystem->Load("/home/tigress/new_packages/GRSISpoon/libraries/libTigress.so");
+	gSystem->Load("/home/bender/Programs/GRSISpoon/libraries/libTigress.so");
 	//gStyle->SetOptStat(111111);
 	//gROOT->LoadMacro("Setup.C");
 }
---
+++ testSuite/testGauntlet_v2.C
@@ -0,0 +1,160 @@
+
+#include "../include/Globals.h" // for color
+
+void testGauntlet()	{
+
+	//open our files
+	TFile *standard  = new TFile("fragment27422_000_benchmark.root");
+	printf("\t" BLUE  "standard  file opened: %s" RESET_COLOR "\n", standard->GetName());
+	TFile *contender = new TFile("fragment27422_000.root");
+	printf("\t" RED   "contender file opened: %s" RESET_COLOR "\n", contender->GetName());
+
+	
+	open our trees
+	TTree *benchmarkTree = (TTree*)standard->Get("FragmentTree");
+	TTree *contenderTree = (TTree*)contender->Get("FragmentTree");
+
+	TTigFragment *frag_bench = 0;
+	TTigFragment *frag_contd = 0;
+
+	benchmarkTree->SetBranchAddress("TTigFragment",&frag_bench);
+	contenderTree->SetBranchAddress("TTigFragment",&frag_contd);
+
+	Int_t nentries_st = (Int_t)benchmarkTree->GetEntries();
+	Int_t nentries_ct = (Int_t)contenderTree->GetEntries();
+
+
+	if(nentries_ct != nentries_st)	{
+		printf("warning standrad  tree has " BLUE "%i fragments!" RESET_COLOR "\n"  ,nentries_st;);
+		printf("warning contender tree has " RED  "%i fragments!" RESET_COLOR "\n"  ,nentries_ct;);
+		return;
+	}
+
+	for(Int_t i=0; i<nentries_st;i++)	{
+		benchmarkTree->GetEntry(i);
+		contenderTree->GetEntry(i);
+
+		int status = CompareFragments(frag_bench,frag_contd);
+		if(status<0) 
+			printf("\terror on entry number: %i!\n",nentries);
+
+
+
+	}
+	
+}
+
+          
+int CompareFragments(TTigFragment *frag1, TTigFragment *frag2)	{
+	if(!frag1 || !frag2)	{
+		printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->MidasTimeStamp	!=	  frag2->MidasTimeStamp)	{	
+		printf("TimeStamps do not match\n");
+		return -1;				
+	}
+
+    	if(frag1->MidasId		!=        frag2->MidasId)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TriggerId		!=        frag2->TriggerId)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->FragmentId		!=        frag2->FragmentId)	{
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TriggerBitPattern	!=        frag2->TriggerBitPattern)	{
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->DigitizerType		!=        frag2->DigitizerType)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->ODBType		!=        frag2->ODBType)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+                                                                               
+
+	if(frag1->ChannelName		!=	  frag2->ChannelName)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->ChannelNumber		!=        frag2->ChannelNumber)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->ChannelRaw		!=        frag2->ChannelRaw)	{
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->Cfd			!=        frag2->Cfd)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->Led			!=        frag2->Led)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TimeToTrig		!=        frag2->TimeToTrig)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->Charge		!=        frag2->Charge)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->ChargeCal		!=        frag2->ChargeCal)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+                                                                                
+	if(frag1->TimeStampLow		!=        frag2->TimeStampLow)	{	
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TimeStampHigh		!=        frag2->TimeStampHigh)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TimeStampLive		!=        frag2->TimeStampLive)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TimeStampTR		!=        frag2->TimeStampTR)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+	if(frag1->TimeStampTA		!=        frag2->TimeStampTA)	{		
+		//printf("One fragment not set!\n");
+		return -1;				
+	}
+
+
+
+
+}
+
+
