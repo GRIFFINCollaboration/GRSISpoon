453602340720518054de0e35e045d332e7f84e15
BillMills <mills.wj@gmail.com>
2014-02-16 15:26:41 -0800

    completed footer for web interface

--- IPython/custom.js
+++ IPython/custom.js
@@ -30,7 +30,8 @@ $([IPython.events]).on('notebook_loaded.Notebook', function(){
 	//TODO: really awful, figure out proper callback:
         window.setTimeout(function(){
             //branding
-            GRSISpoon('GRSIlogo', 20);
+            //GRSISpoon('GRSIlogo', 20);
+            footerImage('GRSIlogo', 2, '#DDDDDD');
 
 	    //populate form options
             getListOfVariables();
@@ -43,14 +44,20 @@ $([IPython.events]).on('notebook_loaded.Notebook', function(){
             var footer = document.querySelector('.end_space'),
                 footerTableHTML;
 
-            footerTableHTML = "<table><tr><td><img src='static/custom/GitHub-Mark-32px.png'></img></td><td>hosted</td></tr>";
-            footerTableHTML += "<tr><td><img src='static/custom/GitHub-Mark-32px.png'></img></td><td>powered</td></tr>";
-            footerTableHTML += "<tr><td><img src='static/custom/GitHub-Mark-32px.png'></img></td><td>created</td></tr></table>"                
+            footerTableHTML = "<div class='textBlock'><h3>Built in Vancouver by the GRIFFIN Collaboration</h3>"
+            footerTableHTML += "<p>Code available on <a href='https://github.com/GRIFFINCollaboration/GRSISpoon'>Github</a><br>"
+            footerTableHTML += "Web interface powered by the <a href='http://ipython.org/notebook.html'>ipython notebook</a><br>"
+            footerTableHTML += "Copyright &#169 2014 GRIFFIN Collaboration<br>"
+            footerTableHTML += "All code freely available under MIT license.</p>"
+            footerTableHTML += "<table><tr><td><a class='imgLink' href='https://github.com/GRIFFINCollaboration/GRSISpoon'><img width='55' height='55' src='static/custom/GitHub-Mark-Light-64px.png'></img></a></td>"
+            footerTableHTML += "<td><a class='imgLink' href='http://ipython.org/notebook.html'><img width='113' height='40' src='static/custom/ipyLogo.png'></img></a></td>"
+            footerTableHTML += "<td><a class='imgLink' href='http://trshare.triumf.ca/~garns/GRIFFIN/'><img width='65' height='50' src='static/custom/GRIFFIN_Logo_White_small.png'></img></a></td></tr></table></div>"
+
             footerTableHTML += "<canvas id='footerImage' width='550' height='300'></canvas>";
 
             footer.innerHTML = footerTableHTML;
 
-            footerImage('footerImage', 2);
+            footerImage('footerImage', 2, '#333333');
 
             //fade everything in after setup
             document.getElementById('notebook').style.opacity = 1;
--- IPython/util.js
+++ IPython/util.js
@@ -67,11 +67,14 @@ define(function(){
 	    context.fillText('GRSISpoon', 0.2*size, 2.8*size)
 	}
 
-    var footerImage = function(canvasID, size){
+    var footerImage = function(canvasID, size, color){
             var canvas = document.getElementById(canvasID),
                 context = canvas.getContext('2d'),
-                isotopesPerRow = [1,6,8,8,9,13,15,15,15,16,18,19,19,20,21,23,24,24,22,23,21,20,21,25,25,26,28,28,30,29,30,31,32,33,30,30,32,31],  //Rb last 
+                //how many isotopes are there of each element
+                isotopesPerRow = [1,6,8,8,9,13,15,15,15,16,18,19,19,20,21,23,24,24,22,23,21,20,21,25,25,26,28,28,30,29,30,31,32,33,30,30,32,31],  //Rb last
+                //offset of proton drip line from hydrogen 
                 protonDrip = [1,0,1,1,2,2,2,3,4,5,5,6,7,8,8,9,10,11,12,13,14,15,16,17,18,19,19,21,21,23,24,25,26,27,31,33,33,35],
+                //indices of stable isotopes relative to that element's proton drip
                 stable = [
                     [], [0,1], [0,1], [2,3], [3], [3,4], [4,5], [4,5], [4,5,6], [5], [5,6,7],
                     [6], [5,6,7], [6], [6,7,8], [7], [6,7,8,10], [7,9], [6,8,10], [7,9], [6,8,9,10,12],
@@ -79,7 +82,7 @@ define(function(){
                     [13,15], [12,14,15,16,18], [15], [9,11,12,13,15], [11,13], [11,13,14,15,17], [13]
                 ],
                 cell = 4*size,
-                y0 = $('#footerImage').height() - cell/2,
+                y0 = $('#'+canvasID).height() - cell/2,
                 i,j;
 
             for(i=0; i<isotopesPerRow.length; i++){
@@ -88,8 +91,8 @@ define(function(){
                         context.strokeStyle = '#FF3399';
                         context.fillStyle = '#FF3399';
                     } else{
-                       context.strokeStyle = '#333333';
-                       context.fillStyle = '#333333';
+                       context.strokeStyle = color;
+                       context.fillStyle = color;
                     }
                     context.beginPath();
                     context.arc(cell*protonDrip[i] + cell/2 + j*cell, y0, size, 0, 2*Math.PI);
@@ -98,6 +101,7 @@ define(function(){
                     context.stroke();
                 }
                 y0 -= cell;
+                if(y0<0) return;
             }
 
         }
