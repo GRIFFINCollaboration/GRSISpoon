7b87c0758247800602c0cd2b315bce17c5d05348
P.C. Bender <pcbend@gmail.com>
2014-02-16 14:03:29 -0800

    put a built fragment check in the test suite.

--- testSuite/rootlogon.C
+++ testSuite/rootlogon.C
@@ -1,6 +1,6 @@
 {
 	gSystem->Load("$ROOTSYS/lib/libPhysics.so");   // need to make tvector3 play nice.
-	gSystem->Load("libFormat.so");
+	gSystem->Load("$GRSISYS/libraries/libFormat.so");
 	//gStyle->SetOptStat(111111);
 	//gROOT->LoadMacro("Setup.C");
 }
