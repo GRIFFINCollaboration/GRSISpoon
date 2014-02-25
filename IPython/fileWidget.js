20789a22ec015fd8e654eac4a88a97639fee8863
BillMills <mills.wj@gmail.com>
2014-02-24 16:08:38 -0800

    fixed weird whitespace

--- IPython/fileWidget.js
+++ IPython/fileWidget.js
@@ -18,54 +18,54 @@ function toggleElt(elt){
     gradientList();        
 }
     
-    //double click insta-toggles:
-    function fileDoubleClick(event){
-            toggleElt(event.target);  
-            return;                                    
-    }
+//double click insta-toggles:
+function fileDoubleClick(event){
+        toggleElt(event.target);  
+        return;                                    
+}
 
-    //single click queues or unqueues for toggling:
-    function fileClick(event){
-        var fileElts, inRange;
-        
-        //just vanilla if no shift:
-        if(!event.shiftKey){
+//single click queues or unqueues for toggling:
+function fileClick(event){
+    var fileElts, inRange;
+    
+    //just vanilla if no shift:
+    if(!event.shiftKey){
+        if(event.target.className.indexOf('fileSelected') == -1)
+            event.target.className += ' fileSelected';
+        else 
+            event.target.className = event.target.className.slice(0, ' ');
+    //shift highlights a range of elements
+    } else{
+        //if there's a boundary chosen, select everything between it and this element
+        if(window.fileRangeBoundary){
+            inRange = false;
+            fileElts = document.querySelectorAll('ul#'+event.target.parentNode.id+' li');
+            [].forEach.call(fileElts, function(elt){
+                //here we are at the start of the run of selected items:
+                if(!inRange && (elt == window.fileRangeBoundary || elt == event.target) ){
+                    inRange = true;
+                    if(elt.className.indexOf('fileSelected') == -1)
+                        elt.className += ' fileSelected';
+                //at the end of the run:
+                } else if(inRange && (elt == window.fileRangeBoundary || elt == event.target)){
+                    inRange = false;
+                    if(elt.className.indexOf('fileSelected') == -1)
+                        elt.className += ' fileSelected';
+                //in the middle of the run:
+                } else if(inRange){
+                    if(elt.className.indexOf('fileSelected') == -1)
+                        elt.className += ' fileSelected';                                       
+                }
+            })
+            window.fileRangeBoundary = null;
+        //this must be the first of 2 boundaries, keep hold of it for later.
+        } else {
+            window.fileRangeBoundary = event.target;
             if(event.target.className.indexOf('fileSelected') == -1)
-                event.target.className += ' fileSelected';
-            else 
-                event.target.className = event.target.className.slice(0, ' ');
-        //shift highlights a range of elements
-        } else{
-            //if there's a boundary chosen, select everything between it and this element
-            if(window.fileRangeBoundary){
-                inRange = false;
-                fileElts = document.querySelectorAll('ul#'+event.target.parentNode.id+' li');
-                [].forEach.call(fileElts, function(elt){
-                    //here we are at the start of the run of selected items:
-                    if(!inRange && (elt == window.fileRangeBoundary || elt == event.target) ){
-                        inRange = true;
-                        if(elt.className.indexOf('fileSelected') == -1)
-                            elt.className += ' fileSelected';
-                    //at the end of the run:
-                    } else if(inRange && (elt == window.fileRangeBoundary || elt == event.target)){
-                        inRange = false;
-                        if(elt.className.indexOf('fileSelected') == -1)
-                            elt.className += ' fileSelected';
-                    //in the middle of the run:
-                    } else if(inRange){
-                        if(elt.className.indexOf('fileSelected') == -1)
-                            elt.className += ' fileSelected';                                       
-                    }
-                })
-                window.fileRangeBoundary = null;
-            //this must be the first of 2 boundaries, keep hold of it for later.
-            } else {
-                window.fileRangeBoundary = event.target;
-                if(event.target.className.indexOf('fileSelected') == -1)
-                    event.target.className += ' fileSelected';                
-            }
+                event.target.className += ' fileSelected';                
         }
     }
+}
 
 //toggle all the selected files
 function toggleQueuedFiles(){
@@ -81,42 +81,42 @@ function toggleQueuedFiles(){
     })        
 }
 
-    function gradientList(){
-        var stepSize = 10,
-            initUnselected = 175,
-            lastUnselected = 255,
-            unselectedSteps = (lastUnselected - initUnselected) / stepSize,
-            initSelectedRed = 5,
-            initSelectedGreen = 195,
-            initSelectedBlue = 105,
-            lastSelectedRed = 65,
-            lastSelectedGreen = 255,
-            lastSelectedBlue = 165,
-            selectedSteps = (lastSelectedRed - initSelectedRed) / stepSize,
-            unselectedFiles = document.querySelectorAll('ul#unselectedFiles li'),
-            selectedFiles = document.querySelectorAll('ul#selectedFiles li'),
-            index = 0,
-            increment = 1;
-            
-        
-        [].forEach.call(unselectedFiles, function(elt){
-            if(elt.className != 'fileHidden'){
-                elt.style.backgroundColor = 'rgba(' + (initUnselected + stepSize*index) + ', ' + (initUnselected + stepSize*index) + ', ' + (initUnselected + stepSize*index) + ', 1)';
-                index += increment;
-                if(index == unselectedSteps || index == 0)
-                    increment *= -1;
-            }
-        })        
-        
-        index = 0;
+function gradientList(){
+    var stepSize = 10,
+        initUnselected = 175,
+        lastUnselected = 255,
+        unselectedSteps = (lastUnselected - initUnselected) / stepSize,
+        initSelectedRed = 5,
+        initSelectedGreen = 195,
+        initSelectedBlue = 105,
+        lastSelectedRed = 65,
+        lastSelectedGreen = 255,
+        lastSelectedBlue = 165,
+        selectedSteps = (lastSelectedRed - initSelectedRed) / stepSize,
+        unselectedFiles = document.querySelectorAll('ul#unselectedFiles li'),
+        selectedFiles = document.querySelectorAll('ul#selectedFiles li'),
+        index = 0,
         increment = 1;
-        [].forEach.call(selectedFiles, function(elt){
-            if(elt.className != 'fileHidden'){
-                elt.style.backgroundColor = 'rgba(' + (initSelectedRed + stepSize*index) + ', ' + (initSelectedGreen + stepSize*index) + ', ' + (initSelectedBlue + stepSize*index) + ', 1)';
-                index += increment;
-                if(index == selectedSteps || index == 0)
-                    increment *= -1;            
-            }
-        })
+        
+    
+    [].forEach.call(unselectedFiles, function(elt){
+        if(elt.className != 'fileHidden'){
+            elt.style.backgroundColor = 'rgba(' + (initUnselected + stepSize*index) + ', ' + (initUnselected + stepSize*index) + ', ' + (initUnselected + stepSize*index) + ', 1)';
+            index += increment;
+            if(index == unselectedSteps || index == 0)
+                increment *= -1;
+        }
+    })        
+    
+    index = 0;
+    increment = 1;
+    [].forEach.call(selectedFiles, function(elt){
+        if(elt.className != 'fileHidden'){
+            elt.style.backgroundColor = 'rgba(' + (initSelectedRed + stepSize*index) + ', ' + (initSelectedGreen + stepSize*index) + ', ' + (initSelectedBlue + stepSize*index) + ', 1)';
+            index += increment;
+            if(index == selectedSteps || index == 0)
+                increment *= -1;            
+        }
+    })
 
-    }
+}
